a0dc57d49f8d58b618aef498c44404f9463b67ce
Gustavo Arruda <151965666+GustavoArruda1@users.noreply.github.com>
2025-04-05 19:39:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <?php
     
     $result = '';
+    $resultadoFinal = '';
+    session_start();
 
     if (isset($_POST['acao'])) {
         if (is_numeric($_POST['num1']) && is_numeric($_POST['num2'])) {
@@ -20,14 +22,17 @@
                 case '+':
                     $soma = $_POST['num1'] + $_POST['num2'];
                     $result = $soma;
+                    $resultadoFinal = $_POST['num1'] . ' ' . $_POST['operacao'] . ' ' . $_POST['num2'] . ' = ' . $result;
                     break;
                 case '-':
                     $subtracao = $_POST['num1'] - $_POST['num2'];
                     $result = $subtracao;
+                    $resultadoFinal = $_POST['num1'] . ' ' . $_POST['operacao'] . ' ' . $_POST['num2'] . ' = ' . $result;
                     break;
                 case '*':
                     $multiplicacao = $_POST['num1'] * $_POST['num2'];
                     $result = $multiplicacao;
+                    $resultadoFinal = $_POST['num1'] . ' ' . $_POST['operacao'] . ' ' . $_POST['num2'] . ' = ' . $result;
                     break;
                 case '/':
                     if ($_POST['num2'] == 0) {
@@ -35,6 +40,7 @@
                     } else {
                         $divisao = $_POST['num1'] / $_POST['num2'];
                         $result = $divisao;
+                        $resultadoFinal = $_POST['num1'] . ' ' . $_POST['operacao'] . ' ' . $_POST['num2'] . ' = ' . $result;
                     }
                     break;
         }   
@@ -43,40 +49,54 @@
         }
     }
 
+    if (isset($_POST['clear'])) {
+        $result = '';
+        $_POST['num1'] = '';
+        $_POST['num2'] = '';
+    }
+
     ?>
 
     <div class="calc">
         <div class="resultado">
             <p>
-                <?php echo $result; ?>
+                <?php echo $resultadoFinal; ?>
             </p>
         </div>
         <form method="post">
             <div class="input-container">
                 <div class="input-item">
-                    <input class="input-text" id="2" type="text" name="num2" required>
+                    <input class="input-text" value="<?php echo isset($_POST['num2']) ? $_POST['num2'] : ''; ?>" id="2"
+                        type="text" name="num2" required>
                     <label for="2">Digite o segundo número</label>
                 </div> <!-- input-item -->
                 <div class="input-item">
-                    <input id="1" class="input-text" type="text" name="num1" required>
+                    <input id="1" class="input-text" value="<?php echo isset($_POST['num1']) ? $_POST['num1'] : ''; ?>"
+                        type="text" name="num1" required autofocus>
                     <label for="1">Digite o primeiro número</label>
 
                 </div> <!-- input-item -->
                 <div class="opr">
                     <label for="3">Selecione a operação</label>
                     <select name="operacao" id="3">
-                        <option name="+" value="+">Adição (+)</option>
-                        <option value="-">Subtração (-)</option>
-                        <option value="*">Multiplicação (*)</option>
-                        <option value="/">Divisão (/)</option>
+                        <option name="+" <?php if (isset($_POST['operacao']) && $_POST['operacao']=='+' )
+                            echo 'selected' ; ?>>Adição (+)</option>
+                        <option value="-" <?php if (isset($_POST['operacao']) && $_POST['operacao']=='-' )
+                            echo 'selected' ; ?>>Subtração (-)</option>
+                        <option value="*" <?php if (isset($_POST['operacao']) && $_POST['operacao']=='*' )
+                            echo 'selected' ; ?>>Multiplicação (*)</option>
+                        <option value="/" <?php if (isset($_POST['operacao']) && $_POST['operacao']=='/' )
+                            echo 'selected' ; ?>>Divisão (/)</option>
                     </select>
                 </div> <!-- opr -->
             </div> <!-- input-container -->
             <div class="enviar">
-                <input type="submit" name="acao" value="=">
+                <input class="igual" type="submit" name="acao" value="=">
+                <input class="clear" type="submit" name="clear" value="c">
             </div> <!-- enviar -->
         </form>
-    </div>
+    </div> <!-- calc -->
+
 </body>
 
 </html>
